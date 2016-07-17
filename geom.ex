9a8a5c073cdd394463f86f24f29c4c514096b02b
Raymund Lew <raymund.lew@gmail.com>
2016-07-16 21:10:13 -0700

    first commit...practicing using elixir etudes

---
+++ geom.ex
@@ -0,0 +1,13 @@
+# geom.ex
+defmodule Geom do
+  @moduledoc """
+  Provides geometric related functions
+  """
+
+  @doc """
+  Calculates the area
+  """
+  def area(a \\ 1, b \\ 1) do
+    a * b
+  end
+end
