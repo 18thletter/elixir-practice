573edcf7c7d03aed91683f752ea199e339621339
Raymund Lew <raymund.lew@gmail.com>
2016-07-16 22:26:55 -0700

    updated docs on geom

--- geom.ex
+++ geom.ex
@@ -2,12 +2,26 @@
 defmodule Geom do
   @moduledoc """
   Provides geometric related functions
+
+  # Functions
+  area(number, number) :: number
   """
   @vsn 0.1
 
   @doc """
-  Calculates the area
+  Calculates the area given a length and width.
+  Both length and width default to 1.
+
+  # Examples
+  ```
+    iex> Geom.area(1,4)
+    4
+
+    iex> Geom.area(14)
+    14
+  ```
   """
+  @spec area(number, number) :: number
   def area(a \\ 1, b \\ 1) do
     a * b
   end
