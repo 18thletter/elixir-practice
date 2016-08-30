eb54e1e943b336145e457bf614fb316585102930
Raymund Lew <raymund.lew@gmail.com>
2016-08-30 14:57:38 -0700

    Etude 3-2 added guards

--- geom.ex
+++ geom.ex
@@ -22,15 +22,15 @@ defmodule Geom do
   ```
   """
   @spec area(atom, number, number) :: number
-  def area(:rectangle, a, b) do
+  def area(:rectangle, a, b) when a >= 0 and b >= 0 do
     a * b
   end
-  
-  def area(:triangle, a, b) do
+
+  def area(:triangle, a, b) when a >= 0 and b >= 0 do
     a * b / 2.0
   end
 
-  def area(:ellipse, a, b ) do
+  def area(:ellipse, a, b ) when a >= 0 and b >= 0 do
     :math.pi() * a * b
   end
 end
