ecca3cd66807e16398084b2483a966aca2fdcafa
Raymund Lew <raymund.lew@gmail.com>
2016-08-30 20:08:55 -0700

    Etude 4-2 dijkstra

---
+++ dijkstra.ex
@@ -0,0 +1,13 @@
+defmodule Dijkstra do
+  @spec gcd(integer, integer) :: integer
+  def gcd(m, n) do
+    cond do
+      m == n ->
+        m
+      m > n ->
+        gcd(m - n, n)
+      true ->
+        gcd(m, n - m)
+    end
+  end
+end
